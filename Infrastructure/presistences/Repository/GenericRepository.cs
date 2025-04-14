fd4530c5f58f261da3693939991cf29f0d63eeed
Mohammedyassin22 <moyassin2004522@gmail.com>
2025-04-14 11:09:17 +0200

    Create BAL and Unit of Work

---
+++ Infrastructure/presistences/Repository/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Contracts;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using presistences.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace presistences.Repository
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly StoreDbContext _dbContext;
+        public GenericRepository(StoreDbContext context) {
+        _dbContext = context;
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbContext.AddAsync(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbContext.Remove(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsunc(bool trackchanges = false)
+        {
+            return trackchanges ?
+                 await _dbContext.Set<TEntity>().ToListAsync():
+                await _dbContext.Set<TEntity>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<TEntity?> GetAsync(TKey id)
+        {
+            return await _dbContext.Set<TEntity>().FindAsync(id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbContext.Update(entity);
+        }
+    }
+}
